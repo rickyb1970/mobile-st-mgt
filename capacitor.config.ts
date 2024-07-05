e5b6745247c3abe4e0776d12e4bba3406e060ee6
[Roderick Bandalan] <[ricky.bandalan@gmail.com]>
2024-07-06 02:48:24 +0800

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'mobile-st-mgt',
+  webDir: 'www'
+};
+
+export default config;
